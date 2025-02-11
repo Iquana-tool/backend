76edc2cd863bc0d52081cb42e27317e9fc6570bb
Robert Leist <robert.leist@dfki.de>
2025-02-11 08:31:36 +0100

    Added config file

---
+++ app/config.py
@@ -0,0 +1,44 @@
+import os
+
+
+class Paths:
+    """ This class contains the paths to the project directories. It can be used to access the paths from
+    any file in the project.
+    Attributes:
+        base_dir (str): The base directory of the project.
+        data_dir (str): The data directory of the project.
+        meso_dir (str): The directory of the meso-scale images of corals.
+        micro_dir (str): The directory of the micro-scale CT Scans of corals.
+        app_dir (str): Contains all folders concerning the application.
+        database_dir (str): Files defining the database scheme of the app.
+        routes_dir (str): The directory containing the routes of the app. This manages endpoints and requests.
+        schemas_dir (str): The directory containing the schemas of endpoint request/ response data formats.
+        services_dir (str): The directory containing the functionality of the app.
+        weights_dir (str): The directory containing the weights of backend models.
+        """
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+
+    data_dir = os.path.join(base_dir, 'data')
+    meso_dir = os.path.join(data_dir, 'meso-scale')
+    micro_dir = os.path.join(data_dir, 'micro-scale')
+
+    app_dir = os.path.join(base_dir, 'app')
+    database_dir = os.path.join(app_dir, 'database')
+    routes_dir = os.path.join(app_dir, 'routes')
+    schemas_dir = os.path.join(app_dir, 'schemas')
+    services_dir = os.path.join(app_dir, 'services')
+    weights_dir = os.path.join(services_dir, 'weights')
+
+
+
+class ModelConfig:
+    """ This class contains the configuration options for the model. """
+    class MesoModel:
+        """ This class contains the configuration options for the Mesoscale model. """
+        weights = os.path.join(Paths.weights_dir, 'sam2.1_hiera_large.pt')
+        config = os.path.join(Paths.weights_dir, 'sam2.1_hiera_l.yaml')
+
+    class MicroModel:
+        """ This class contains the configuration options for the Microscale model. """
+        weights = os.path.join(Paths.weights_dir, 'sam2.1_hiera_large.pt')
+        config = os.path.join(Paths.weights_dir, 'sam2.1_hiera_l.yaml')
