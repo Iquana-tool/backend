a5605cb20fb7a080a206fbfa45eb61924c7752b1
Robert Leist <robert.leist@dfki.de>
2025-02-24 15:56:05 +0100

    add database engine and session management setup

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,3 +1,24 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from contextlib import contextmanager
+from config import Paths
 
+# Define the declarative base
 database = declarative_base()
+
+# Create an engine
+engine = create_engine(Paths.database)
+
+database.metadata.create_all(engine)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Context manager for session handling
+@contextmanager
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
