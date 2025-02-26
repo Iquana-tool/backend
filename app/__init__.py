2efdf22a379c69ab43e1b99b8d06eb579caf2965
Robert Leist <robert.leist@dfki.de>
2025-02-26 14:49:34 +0100

    Initialize directories and database during app creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+import config
 from app.routes.segmentation import router as segmentation_router
 from app.routes.images import router as image_router
+from app.database import init_db
 
 
 def create_app():
+    # Initialize the directories
+    for directory in [directory for directory in dir(config.Paths) if "dir" in directory and not "__" in directory]:
+        os.makedirs(getattr(config.Paths, directory), exist_ok=True)
+
+    init_db()
+
     app = FastAPI(
         title="Coral Segmentation API",
         description="FastAPI backend for interactive coral segmentation",
