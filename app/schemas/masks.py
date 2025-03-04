c89c4a44b0beb8122c31ca87a2b2d6c8ecbaee10
Robert Leist <robert.leist@dfki.de>
2025-03-04 16:23:34 +0100

    started working on masks

--- app/schemas/masks.py
+++ app/schemas/masks.py
@@ -2,8 +2,10 @@ from pydantic import BaseModel, Field, field_validator
 from app.database.images import Images
 from app.database import get_context_session
 import numpy as np
+from typing import List, Annotated
 
 
 class MaskRequest(BaseModel):
-    rle_mask: str
-    label: str
+    base64_mask: str
+    label: Annotated[str, "A name for what the mask represents."]
+    image_id: int
