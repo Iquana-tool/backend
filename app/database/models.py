fd2ab37eb621d373bf88a793c3bc2b7c4ab42629
Robert Leist <robert.leist@dfki.de>
2025-03-06 08:48:19 +0100

    adding models table, which is unused currently, but could be used to give the frontend the available models

---
+++ app/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from . import database
+
+
+class Models(database):
+    __tablename__ = 'models'
+    id = Column(String, primary_key=True, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    version = Column(String, nullable=False)
+    created_at = Column(String, nullable=False)
+    updated_at = Column(String, nullable=False)
