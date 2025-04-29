8cdb653348e3e0e4c355a6ab71637e71cf0b4252
Robert Leist <robert.leist@dfki.de>
2025-04-29 14:51:12 +0200

    move basemodel to its own file to avoid circular import

--- app/services/segmentation/__init__.py
+++ app/services/segmentation/__init__.py
@@ -1,38 +1,10 @@
 import config
 from logging import getLogger
+from app.services.segmentation.base_model import SegmentationBaseModel
 from app.services.segmentation.mockup import MockupSegmentationModel
 from app.services.segmentation.sam2 import SAM2
-from app.schemas.segmentation_and_masks import SegmentationRequest
-from sqlalchemy.orm import Session
-
 
 logger = getLogger(__name__)
-
-
-class SegmentationBaseModel:
-    """Base class for segmentation models.
-    """
-    def __init__(self):
-        self.model = None
-
-    def process_request(self, request: SegmentationRequest) -> tuple[list, list]:
-        """Process the segmentation request. This function calls the prepare_input and segment methods sequentially.
-        This allows for individual preparation methods for each model. Additionally, the segment method can be
-        overridden in subclasses to provide model-specific segmentation logic like prompted or unprompted segmentation.
-        Each model can also override this method to allow for even more flexibility.
-        """
-        logger.debug(f"Processing segmentation request: {request}")
-        return self.segment(**self.prepare_input(**request.dict()))
-
-    def prepare_input(self, **kwargs):
-        """Prepare the image for the model.
-        """
-        raise NotImplementedError("Subclasses should implement this method.")
-
-    def segment(self, **kwargs) -> tuple[list, list]:
-        """Segment the given input.
-        """
-        raise NotImplementedError("Subclasses should implement this method.")
 
 
 def get_model_via_identifier(identifier: str) -> SegmentationBaseModel:
