91b119ec9cea0995608cc156db0c2ff866840301
Robert Leist <robert.leist@dfki.de>
2025-04-29 10:48:57 +0200

    added basic postprocessing to the segmentation step which is toggleable via the segmentation request

---
+++ app/services/postprocessing.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+
+
+def postprocess_binary_mask(mask: np.ndarray):
+    """Post-process a binary mask by removing small objects and filling holes."""
+    # Ensure mask is binary
+    if np.unique(mask).size > 2:
+        raise ValueError("Input mask is not binary. Postprocessing requires a binary mask.")
+
+    # Fill holes via Closing
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+
+    return mask
