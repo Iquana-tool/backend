89e93b0ddeaecf20a9b31d8870a4df1166ce8dc5
Robert Leist <robert.leist@dfki.de>
2025-04-29 14:48:33 +0200

    added some logging

--- app/routes/segmentation.py
+++ app/routes/segmentation.py
@@ -1,21 +1,15 @@
 import logging
-
-import cv2
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
-import config
 from app.database import get_session
-from app.database.images import ImageEmbeddings, Images
 from app.schemas.segmentation_and_masks import (
     SegmentationRequest, SegmentationResponse, ContourModel, 
     SegmentationMaskModel, QuantificationsModel
 )
-from app.services.database_access import load_image_as_array_from_disk, load_embedding, save_embedding
-from app.services.prompts import Prompts
 from app.services.segmentation import get_model_via_identifier
 from app.services.contours import get_contours
 from app.services.quantifications import Contour
+from app.services.database_access import get_height_width
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -28,8 +22,10 @@ router = APIRouter(prefix="/segmentation", tags=["segmentation"])
 async def segment_image(request: SegmentationRequest, db: Session = Depends(get_session)):
     """Perform segmentation with optional prompts, using data validation."""
     model = get_model_via_identifier(request.model)
-    logger.debug(f"Using model: {model.__name__}")
+    logger.debug(f"Using model: {model.model_name}")
     masks, quality = model.process_request(request)
+    logger.debug(f"Segmentation completed for image_id: {request.image_id} with {len(masks)} masks.")
+    height, width = get_height_width(request.image_id)
     masks_response = []
     for mask, quality in zip(masks, quality):
         contours = get_contours(mask)
