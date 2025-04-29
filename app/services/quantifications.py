119995b808469034d650f80369e184640d49dc9c
Robert Leist <robert.leist@dfki.de>
2025-04-29 10:37:57 +0200

    decreased number of diameters to compute to improve performance

--- app/services/quantifications.py
+++ app/services/quantifications.py
@@ -30,7 +30,7 @@ class Contour:
         else:
             self.circularity = (4 * np.pi * self.area) / (self.perimeter ** 2)
 
-    def get_diameters(self, step_size=100):
+    def get_diameters(self, step_size=25):
         """ Get the diameters of the objects in the mask.
         Args:
             step_size (int): The number of times to measure the diameter. This will measure the diameter each
