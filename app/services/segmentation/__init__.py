7ff838a0586ad0df51f107a18a06b0d76f9bde6d
Robert Leist <robert.leist@dfki.de>
2025-04-29 12:12:15 +0200

    simplified segmentation base model

--- app/services/segmentation/__init__.py
+++ app/services/segmentation/__init__.py
@@ -2,6 +2,8 @@ import config
 from logging import getLogger
 from app.services.segmentation.mockup import MockupSegmentationModel
 from app.services.segmentation.sam2 import SAM2
+from app.schemas.segmentation_and_masks import SegmentationRequest
+from sqlalchemy.orm import Session
 
 
 logger = getLogger(__name__)
@@ -13,23 +15,27 @@ class SegmentationBaseModel:
     def __init__(self):
         self.model = None
 
-    def embed_image(self, image):
-        """Embed the image using the model.
+    def process_request(self, request: SegmentationRequest) -> tuple[list, list]:
+        """Process the segmentation request. This function calls the prepare_input and segment methods sequentially.
+        This allows for individual preparation methods for each model. Additionally, the segment method can be
+        overridden in subclasses to provide model-specific segmentation logic like prompted or unprompted segmentation.
+        Each model can also override this method to allow for even more flexibility.
+        """
+        logger.debug(f"Processing segmentation request: {request}")
+        return self.segment(**self.prepare_input(**request.dict()))
+
+    def prepare_input(self, **kwargs):
+        """Prepare the image for the model.
         """
         raise NotImplementedError("Subclasses should implement this method.")
 
-    def segment_with_prompts(self, embedding, size, prompts):
-        """Segment the image using prompts.
-        """
-        raise NotImplementedError("Subclasses should implement this method.")
-
-    def segment_without_prompts(self, image):
-        """Segment the image without prompts.
+    def segment(self, **kwargs) -> tuple[list, list]:
+        """Segment the given input.
         """
         raise NotImplementedError("Subclasses should implement this method.")
 
 
-def get_model_via_identifier(identifier: str):
+def get_model_via_identifier(identifier: str) -> SegmentationBaseModel:
     """
     Returns the model class based on the identifier.
     """
