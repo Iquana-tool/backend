d3c60423471d229badee765197572b5a01ac6eca
Robert Leist <robert.leist@dfki.de>
2025-04-29 12:12:26 +0200

    adapted mockup to simplified segmentation base model

--- app/services/segmentation/mockup.py
+++ app/services/segmentation/mockup.py
@@ -28,46 +28,14 @@ def create_random_mask(height: int, width: int) -> np.ndarray:
 
 
 class MockupSegmentationModel(SegmentationBaseModel):
-    def embed_image(self, image: np.ndarray) -> dict[str, Union[np.ndarray, list[np.ndarray]]]:
-        """ Compute embeddings for image.
-            Args:
-                image (Union[np.ndarray, Image.Image]): The image to embed.
-
-            Returns:
-                dict[str, torch.Tensor]: A dictionary containing the embeddings. The dict has two entries: 'image_embed'
-                 and 'high_res_feats'.
+    def prepare_input(self):
+        """ Does not do anything and returns None.
         """
-        return {}
+        return None
 
-    def segment_with_prompts(self,
-                             embedding: dict,
-                             original_height_width: tuple[int, int],
-                             input_prompts: Prompts):
-        """ Segment an image using prompts.
-            Args:
-                embedding: (dict[str, np.ndarray]): The embedding of the image to segment.
-                original_height_width: (tuple[int, int]): The original height and width of the image.
-                input_prompts (Prompts): The prompts to use for segmentation.
-
-            Returns:
-                A tuple containing a CxHxW array, where C is the number of masks, and an array of length C,
-                 where each entry is the quality of the corresponding mask.
+    def segment(self):
+        """ Returns random masks and scores.
         """
         masks = [create_random_mask(original_height_width[0], original_height_width[1]) for _ in range(3)]
         scores = np.random.rand(len(masks))
         return np.array(masks), scores
-
-    def segment_without_prompts(self, image: np.ndarray):
-        """ Segment an image without prompts.
-            Args:
-                image (Union[np.ndarray, Image.Image]): The image to segment.
-
-            Returns:
-                An array containing the segmentation masks, and an array of the predicted iou scores.
-        """
-        masks = [create_random_mask(image.shape[0], image.shape[1]) for _ in range(3)]
-        scores = np.random.rand(len(masks))
-        return np.array(masks), scores
-
-    def segment_stack(self, stack: np.ndarray, input_prompts: Prompts):
-        raise NotImplementedError("This method is not implemented yet!")
