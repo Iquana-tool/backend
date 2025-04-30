6c565d32cebb9b834e80087b2a3d09d6398d8ba9
Robert Leist <robert.leist@dfki.de>
2025-04-30 09:03:34 +0200

    chanegd kernel size

--- app/services/postprocessing.py
+++ app/services/postprocessing.py
@@ -9,6 +9,8 @@ def postprocess_binary_mask(mask: np.ndarray):
         raise ValueError("Input mask is not binary. Postprocessing requires a binary mask.")
 
     # Fill holes via Closing
-    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+    # Add more methods here
 
     return mask
