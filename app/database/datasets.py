f9d242d2b042a874c6e6ea7e2a3f7e2434119cad
Robert Leist <robert.leist@dfki.de>
2025-05-14 12:16:38 +0200

    removed n_files etc. from db, because it leads to consistency issues.

--- app/database/datasets.py
+++ app/database/datasets.py
@@ -8,9 +8,6 @@ class Datasets(database):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     description = Column(String(255), nullable=True)
-    n_files = Column(Integer, nullable=False, default=0)
-    annotated = Column(Integer, nullable=False, default=0)
-    ai_annotated = Column(Integer, nullable=False, default=0)
 
 
 class Labels(database):
