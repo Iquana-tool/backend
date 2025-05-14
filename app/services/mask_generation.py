dc779ef6d71d7e437d8ce750b0ccffa50cfbe14c
Robert Leist <robert.leist@dfki.de>
2025-05-14 13:16:30 +0200

    refactor generate_mask function to retrieve label_id_to_value dynamically

--- app/services/mask_generation.py
+++ app/services/mask_generation.py
@@ -16,12 +16,14 @@ def build_contour_hierarchy(contours, parent=None):
     return contour_list
 
 
-def generate_mask(mask_id, label_id_to_value: dict[int, int]):
+def generate_mask(mask_id):
     """Generate a mask from the saved contours of that mask."""
     with get_session() as session:
         contours = session.query(Contours).filter_by(mask_id=mask_id).all()
         image_id = session.query(Masks).filter_by(id=mask_id).first().image_id
         image = session.query(Images).filter_by(id=image_id).first()
+        labels = session.query(Labels).filter_by(dataset_id=image.dataset_id).all()
+        label_id_to_value = {label.id: label.value for label in labels}
         canvas = np.zeros((image.height, image.width), dtype=np.uint8)
         for contour in build_contour_hierarchy(contours):
             coords_dict = contour.coords
