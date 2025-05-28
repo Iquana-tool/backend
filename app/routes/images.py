1f386559c6b0b767da84366ce58af0308a7cbf59
Robert Leist <robert.leist@dfki.de>
2025-05-28 12:23:08 +0200

    refactor images.py to enhance scan upload functionality by extracting slice index from filenames, adding folder_path attribute, and improving documentation

--- app/routes/images.py
+++ app/routes/images.py
@@ -1,12 +1,17 @@
 import logging
+import os.path
+import shutil
 
 from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from app.database import get_session
 from app.database.images import Images, Scans
+from app.database.datasets import Datasets
 from app.services.database_access import delete_image_from_disk_and_db, parse_log_file
 from app.services.database_access import save_image_to_disk_and_db, load_image_as_base64_from_disk
+from app.services.util import extract_numbers
+
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/images", tags=["images"])
@@ -102,26 +107,47 @@ async def upload_scan(dataset_id: int,
                       name: str = "Scan",
                       scan_type: str = "CT",
                       description: str = "Scan description",
-                      number_of_slices: int = 0,
                       #meta_data: dict = None,
                       db: Session = Depends(get_session)):
-    """Upload a scan file"""
+    """Upload a scan file.
+    This endpoint allows uploading multiple image files that belong to a scan.
+    It creates a new scan entry in the database and associates the images with it.
+    Args:
+        dataset_id: ID of the dataset to which the scan belongs.
+        files: List of image files to upload. The filenames must include the slice index or number. This number must
+        be present as the last number in the filename to correctly associate the images with the scan. For example:
+        "scan_slice_1.png", "scan_slice_2.png", etc. "Slice_1_scan_19.png" will be associated with index 19.
+        name: Name of the scan.
+        scan_type: Type of scan (e.g., "CT", "MRI"). Optional.
+        description: Description of the scan. Optional.
+        meta_data: Additional metadata about the scan. Optional.
+    Returns:
+        A success message with the IDs of the uploaded images.
+        """
     # First create a new scan entry in the database
     # Then save each image file to disk and the database
     # and associate them with the scan entry
+    dataset = db.query(Datasets).filter_by(id=dataset_id).first()
     new_scan = Scans(
         dataset_id=dataset_id,
         name=name,
         type=scan_type,
         description=description,
-        number_of_slices=number_of_slices,
+        folder_path=os.path.join(dataset.folder_path, name),
+        number_of_slices=len(files),
         #meta_data=meta_data
     )
     db.commit()
     try:
         image_ids = []
         for i, file in enumerate(files):
-            image_id = await save_image_to_disk_and_db(file, dataset_id, new_scan.id, index_in_scan=i)
+            # Extract numbers from the filename. The last number will be used as the index in the scan.
+            numbers_in_filename = extract_numbers(file.filename)
+            # If no numbers are found, use the index as a fallback
+            index_in_scan = i if not numbers_in_filename else numbers_in_filename[-1]
+
+            # Save the image to disk and the database
+            image_id = await save_image_to_disk_and_db(file, dataset_id, new_scan.id, index_in_scan=index_in_scan)
             if image_id is None:
                 raise HTTPException(status_code=400, detail="Invalid file or upload failed")
             image_ids.append(image_id)
@@ -157,7 +183,6 @@ async def upload_scan_with_log_file(
         name=log_data["Filename Prefix"],
         scan_type=scan_type,
         description=description,
-        number_of_slices=len(files),
         #meta_data=log_data,
         db=db
     )
@@ -177,7 +202,8 @@ async def delete_scan(scan_id: int):
             for image in images:
                 delete_image_from_disk_and_db(image.id)
 
-            scan = db.query(Scans).filter(Scans.id == scan_id).first()
+            scan = db.query(Scans).join(Datasets).filter(Scans.id == scan_id).first()
+            shutil.rmtree(os.path.joi)
             db.delete(scan)
             db.commit()
         return {"success": True, "message": f"Deleted scan {scan_id} and its associated images."}
