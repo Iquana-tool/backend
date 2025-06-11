4dd3923d44bffcb9c7ed8338f002fc02cd38ef88
Robert Leist <robert.leist@dfki.de>
2025-06-11 07:59:42 +0200

    refactor segmentation base models to use abstract base class for improved extensibility

--- app/services/segmentation/base_model.py
+++ app/services/segmentation/base_model.py
@@ -1,8 +1,9 @@
 from app.schemas.segmentation.segmentations import (PromptedSegmentationRequest, AutomaticSegmentationRequest,
                                                     ScanPromptedSegmentationRequest, ScanAutomaticSegmentationRequest)
+from abc import ABC, abstractmethod
 
 
-class SegmentationBaseModel:
+class SegmentationBaseModel(ABC):
     """Base class for segmentation models.
     """
     def __init__(self):
@@ -10,11 +11,12 @@ class SegmentationBaseModel:
         self.model_name = None
 
 
-class PromptedSegmentationBaseModel(SegmentationBaseModel):
+class PromptedSegmentationBaseModel(ABC, SegmentationBaseModel):
     """Base class for prompted segmentation models.
     This class is intended for models that require user prompts to perform segmentation.
     It provides a common interface for processing prompted segmentation requests.
     """
+    @abstractmethod
     def process_prompted_request(self, request: PromptedSegmentationRequest) -> tuple[list, list]:
         """Process the segmentation request. Each prompted segmentation model should implement this method.
         Args:
@@ -23,14 +25,15 @@ class PromptedSegmentationBaseModel(SegmentationBaseModel):
             tuple: A tuple containing an array of masks and an array of predicted iou scores. Each mask is a binary mask
             in HW format.
         """
-        raise NotImplementedError("Subclasses should implement this method to process the segmentation request.")
+        pass
 
 
-class AutomaticSegmentationBaseModel(SegmentationBaseModel):
+class AutomaticSegmentationBaseModel(ABC, SegmentationBaseModel):
     """Base class for automatic segmentation models.
     This class is intended for models that perform automatic segmentation without user prompts.
     It provides a common interface for processing automatic segmentation requests.
     """
+    @abstractmethod
     def process_automatic_request(self, request: AutomaticSegmentationRequest) -> tuple[list, list]:
         """Process the automatic segmentation request. Each automatic segmentation model should implement this method.
         Args:
@@ -39,14 +42,15 @@ class AutomaticSegmentationBaseModel(SegmentationBaseModel):
             tuple: A tuple containing an array of masks and an array of predicted iou scores. Each mask is a mask in HW
             format, where each pixel contains its label.
         """
-        raise NotImplementedError("Subclasses should implement this method to process the automatic segmentation request.")
+        pass
 
 
-class PromptedSegmentation3DBaseModel(SegmentationBaseModel):
+class PromptedSegmentation3DBaseModel(ABC, SegmentationBaseModel):
     """Base class for segmentation models that handle scans.
     This class is intended for models that work with scans, such as SAM2.
     It provides a common interface for preparing input and segmenting scans.
     """
+    @abstractmethod
     def process_prompted_segmentation_3D_request(self, request: ScanPromptedSegmentationRequest):
         """Propagate the mask across the scan.
         This method should be overridden by subclasses to provide model-specific mask propagation logic.
@@ -54,11 +58,12 @@ class PromptedSegmentation3DBaseModel(SegmentationBaseModel):
         raise NotImplementedError("Subclasses should implement this method.")
 
 
-class AutomaticSegmentation3DBaseModel(SegmentationBaseModel):
+class AutomaticSegmentation3DBaseModel(ABC, SegmentationBaseModel):
     """Base class for automatic segmentation models that handle scans.
     This class is intended for models that work with scans, such as SAM2.
     It provides a common interface for preparing input and segmenting scans.
     """
+    @abstractmethod
     def process_automatic_segmentation_3D_request(self, request: ScanAutomaticSegmentationRequest) -> tuple[list, list]:
         """Propagate the mask across the scan.
         This method should be overridden by subclasses to provide model-specific mask propagation logic.
