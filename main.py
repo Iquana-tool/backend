04f5c4e1ada13522b2cacfe74bdb8e005acec618
Robert Leist <robert.leist@dfki.de>
2025-06-17 11:48:50 +0200

    trying to fix hydra issues

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import logging
+import hydra
 from app import create_app
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = create_app()
+
+@hydra.main(version_base=None, config_path="./app/services/segmentation/configs", config_name="config")
+def main():
+    app = create_app()
+
+
+if __name__ == "__main__":
+    main()
