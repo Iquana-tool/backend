ebd27c944464279c1ab47003eefe9a54df827d0e
Robert Leist <robert.leist@dfki.de>
2025-06-17 11:54:45 +0200

    didnt work

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from app import create_app
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-@hydra.main(version_base=None, config_path="./app/services/segmentation/configs", config_name="config")
-def main():
-    app = create_app()
+app = create_app()
 
 
 if __name__ == "__main__":
-    main()
+    pass
