75f3e1c996a2cddffd63044a9f2409e52c555dac
Robert Leist <robert.leist@dfki.de>
2025-07-07 16:24:16 +0200

    feat: refactor segmentation services to prompted_segmentation structure and update imports

--- app/routes/automatic_segmentation/training.py
+++ app/routes/automatic_segmentation/training.py
@@ -1,12 +1,18 @@
 from . import router, logger
 from paths import AUTOMATIC_SEGMENTATION_BACKEND_URL as BASE_URL
+from app.schemas.automatic_segmentation.training import TrainingRequest
+import httpx
 
 
 @router.post("/start_training")
-async def start_training():
+async def start_training(request: TrainingRequest):
+    response = await send_start_training_request(request)
     pass
 
 
-async def send_start_training_request():
+async def send_start_training_request(request: TrainingRequest):
     url = f"{BASE_URL}/start_training"
-
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url, data=request)
+        resp.raise_for_status()
+        return resp.json()
