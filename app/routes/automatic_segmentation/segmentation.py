326172add7ca0081dfb7addb1f93e1e35a930e9e
Robert Leist <robert.leist@dfki.de>
2025-07-10 11:58:30 +0200

    using cv2 instead of PIL now

--- app/routes/automatic_segmentation/segmentation.py
+++ app/routes/automatic_segmentation/segmentation.py
@@ -1,3 +1,4 @@
+import cv2
 from fastapi import Depends, APIRouter
 from app.database import get_session
 from sqlalchemy.orm import Session
@@ -21,14 +22,12 @@ router = APIRouter(prefix="/automatic_segmentation", tags=["automatic_segmentati
 @router.post("/segment_batch")
 async def segment_batch_with_backend(model_id: str, image_ids: list[int], db: Session = Depends(get_session)):
     try:
-        model = db.query(Models).filter_by(id=model_id).one()
-        logger.info(f"{model.name} with model_id {model_id} is being used for batch prompted_segmentation of {len(image_ids)} "
-                    f"images.")
         dataset_ids = db.query(Images.dataset_id).filter(Images.id.in_(image_ids)).distinct().all()
         if len(dataset_ids) > 1:
             logger.warning("Batch prompted_segmentation is being performed on images from multiple datasets. "
                            "This may lead to unexpected results.")
-        image_paths = db.query(Images.file_path).filter(Images.id.in_(image_ids)).all()
+        image_paths = list(db.query(Images.file_path).filter(Images.id.in_(image_ids)).all())
+        image_paths = [tup[0] for tup in image_paths]
         response = await send_batch_request(model_id, image_paths)
         zip_bytes = response.content
         # Extract ZIP in-memory
@@ -39,12 +38,12 @@ async def segment_batch_with_backend(model_id: str, image_ids: list[int], db: Se
             for name in names:
                 # Read bytes for each image
                 img_bytes = z.read(name)
-                mask_img = Image.open(BytesIO(img_bytes))
+                mask_img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_GRAYSCALE)
                 masks[name] = mask_img
         masks = list(masks.values())
         mask_responses = await get_masks_responses(masks, np.ones(len(image_ids)).tolist())
-        responses = await create_masks_and_add_contours_for_images(image_ids, mask_responses)
-        failed = len([response.success for response in responses if response["success"]])
+        responses = await create_masks_and_add_contours_for_images(image_ids, mask_responses, db)
+        failed = len([response["success"] for response in responses["responses"] if not response["success"]])
         success = len(image_ids) - failed
         return {
             "success": True,
@@ -54,17 +53,18 @@ async def segment_batch_with_backend(model_id: str, image_ids: list[int], db: Se
 
     except Exception as e:
         logger.error(f"Batch prompted_segmentation failed: {e}")
+        raise e
         return {"success": False, "message": str(e)}
 
 
 async def send_batch_request(model_id: str, image_paths: list[str]) -> dict:
-    url = f"{BASE_URL}/segment_batch"
+    url = f"{BASE_URL}/segment/segment_batch"
     files = [
-        ("files", (p, open(p, "rb"), f"image/{p.split('.')[-1]}"))
+        ("files", (p, open(p, "rb"), "img/octet-stream"))
         for p in image_paths
     ]
     data = {"model_id": model_id}
     async with httpx.AsyncClient() as client:
         resp = await client.post(url, data=data, files=files)
         resp.raise_for_status()
-        return resp.json()
+        return resp
