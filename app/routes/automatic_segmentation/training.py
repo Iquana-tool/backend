6dc7bf5eec0b93da7a657874d38be8d27c2f3625
Robert Leist <robert.leist@dfki.de>
2025-07-11 13:57:49 +0200

    get_training and cancel job work now

--- app/routes/automatic_segmentation/training.py
+++ app/routes/automatic_segmentation/training.py
@@ -14,12 +14,36 @@ logger = getLogger(__name__)
 router = APIRouter(prefix="/automatic_segmentation", tags=["automatic_segmentation"])
 
 
+@router.get("/get_training_status/{job_id}")
+async def get_training_status(job_id: str):
+    url = f"{BASE_URL}/training/get_job_status/{job_id}"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+        return resp.json()
+
+
+@router.get("/cancel_job/{job_id}")
+async def cancel_training_status(job_id: str):
+    try:
+        url = f"{BASE_URL}/training/cancel_job/{job_id}"
+        async with httpx.AsyncClient(timeout=5) as client:
+            resp = await client.get(url)
+            resp.raise_for_status()
+            return resp.json()
+    except httpx.HTTPError as e:
+        return {
+            "success": False,
+            "message": f"Could not cancel job {job_id}, probably due to computational overload."
+        }
+
+
 @router.post("/start_training")
 async def start_training(request: TrainingRequest, db: Session = Depends(get_session)):
     # Get the request as a dict, to add values to it
     request_dict = request.model_dump()
 
-    # Find out the number of classes present.
+    # Find out the number of classes present. + 1 for background
     # Caution: This will get all labels that the user created, even if they did not annotate them anywhere.
     num_classes = db.query(Labels).filter_by(dataset_id=request.dataset_id).count() + 1
     request_dict["num_classes"] = num_classes
@@ -28,7 +52,7 @@ async def start_training(request: TrainingRequest, db: Session = Depends(get_ses
     request_dict["in_channels"] = 3
     request_dict["batch_size"] = 32
     request_dict["lr"] = 0.0001
-    result = await send_start_training_request(request)
+    result = await send_start_training_request(request_dict)
     return JSONResponse(result)
 
 async def send_start_training_request(request: dict):
@@ -36,6 +60,6 @@ async def send_start_training_request(request: dict):
     logger.info(f"Start training request: {request}")
     url = f"{BASE_URL}/training/start_training"
     async with httpx.AsyncClient() as client:
-        resp = await client.post(url, json=request.model_dump())
+        resp = await client.post(url, json=request)
         resp.raise_for_status()
         return resp.json()
