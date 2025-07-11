427ad59555c61304cc6f4fc5fb4b705bd60ac88c
Robert Leist <robert.leist@dfki.de>
2025-07-11 12:43:33 +0200

    adapted training endpoint to be simpler

--- app/routes/automatic_segmentation/training.py
+++ app/routes/automatic_segmentation/training.py
@@ -1,3 +1,7 @@
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from app.database import get_session
+from app.database.datasets import Labels
 from paths import AUTOMATIC_SEGMENTATION_BACKEND_URL as BASE_URL
 from app.schemas.automatic_segmentation.training import TrainingRequest
 import httpx
@@ -11,11 +15,23 @@ router = APIRouter(prefix="/automatic_segmentation", tags=["automatic_segmentati
 
 
 @router.post("/start_training")
-async def start_training(request: TrainingRequest):
+async def start_training(request: TrainingRequest, db: Session = Depends(get_session)):
+    # Get the request as a dict, to add values to it
+    request_dict = request.model_dump()
+
+    # Find out the number of classes present.
+    # Caution: This will get all labels that the user created, even if they did not annotate them anywhere.
+    num_classes = db.query(Labels).filter_by(dataset_id=request.dataset_id).count() + 1
+    request_dict["num_classes"] = num_classes
+
+    # Abstract this from the frontend, user should not be concerned with this.
+    request_dict["in_channels"] = 3
+    request_dict["batch_size"] = 32
+    request_dict["lr"] = 0.0001
     result = await send_start_training_request(request)
     return JSONResponse(result)
 
-async def send_start_training_request(request: TrainingRequest):
+async def send_start_training_request(request: dict):
     """ Forwards to Automatic Segmentation Service."""
     logger.info(f"Start training request: {request}")
     url = f"{BASE_URL}/training/start_training"
--- app/schemas/automatic_segmentation/training.py
+++ app/schemas/automatic_segmentation/training.py
@@ -1,28 +1,23 @@
 import os
 from pydantic import BaseModel, Field, field_validator
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 
 
 class TrainingRequest(BaseModel):
     dataset_id: int = Field(default=1, title="Dataset ID")
-    model_identifier: str = Field(default="unet", description="Identifier for the model to be trained. "
-                                                      "Should be got by GET /models/get_trainable_models")
-    model_id_db: int = Field(description="ID of the model in the database. Must be provided.")
-    restart: bool = Field(default=True, description="Whether to restart training from scratch. If set to True, "
-                                                     "the model will be trained from scratch, else it will continue "
-                                                     "training from the last checkpoint.")
+    model_identifier: Union[int, str] = Field(description="Start training from either a base model, if given a model "
+                                                    "registry key, or from a trained model checkpoint if given "
+                                                   "a model identifier number."
+                                                          "Important: Numbers must be given as int and not as string!")
+    overwrite: bool = Field(default=False, title="Overwrite the existing model or save as a new model. Note: "
+                                                 "Base models cannot be overwritten.")
     epochs: int = Field(default=50, description="Number of epochs to train the model.")
-    batch_size: int = Field(default=64, description="Batch size to train the model.")
-    lr: float = Field(default=0.0001, description="Learning rate to train the model.")
     augment: bool = Field(default=True, description="Whether to augment the dataset. This should be done for small "
                                                     "datasets, but can be left out for bigger datasets.")
     image_size: Optional[Tuple[int, int]] = Field(default=(256, 256), description="Image size to use. Smaller values "
                                                                                   "may lead to faster training, "
                                                                                   "but may also lead to "
                                                                                   "loss of information.")
-    num_classes: int = Field(default=2, description="Number of classes that are present on the masks. Corresponds to the "
-                                                    "number of labels.")
-    in_channels: int = 3
     early_stopping: bool = Field(default=True, description="Whether to use early stopping during training. "
                                                            "This will stop training if the validation loss "
                                                            "does not improve for 5 epochs.")
