fdba5025947ac9df5bff25d2dacaa854dec85bec
Robert Leist <robert.leist@dfki.de>
2025-08-06 11:50:29 +0200

    feat: adding limit parameter to get_contours.

--- app/services/contours.py
+++ app/services/contours.py
@@ -12,11 +12,13 @@ from app.schemas.segmentation.contours_and_quantifications import ContourModel
 logger = getLogger(__name__)
 
 
-def get_contours(mask: np.ndarray, only_return_biggest=False) -> np.ndarray:
+def get_contours(mask: np.ndarray, only_return_biggest=False, limit=200) -> np.ndarray:
     """ Get the contours of the mask.
         Args:
             mask (np.ndarray): The mask to get the contours of.
             only_return_biggest (bool): If True, returns only the biggest contour. If False, returns all contours.
+            limit (int): Specifies how many contours to return at most. If the detected contours surpass this limit,
+                only the largest returns up to the limit will be returned.
 
         Returns:
             np.ndarray: The contours of the mask.
@@ -29,9 +31,9 @@ def get_contours(mask: np.ndarray, only_return_biggest=False) -> np.ndarray:
             return np.array([biggest])
         else:
             contours = sorted(contours, key=cv2.contourArea)
-            if len(contours) > 100:
-                logger.warning("Detected over 100 objects. Only returning the biggest 500 objects.")
-                contours = contours[:100]
+            if len(contours) > limit:
+                logger.warning(f"Detected over {limit} objects. Only returning the biggest 500 objects.")
+                contours = contours[:limit]
             return contours
     else:
         return np.array([])
