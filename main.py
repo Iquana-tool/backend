7bf2efd37d74a5a105ee9b5ad064657915d16f60
Robert Leist <robert.leist@dfki.de>
2025-08-20 08:17:49 +0200

    fix: logs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import logging
+import os
+
 import hydra
 from app import create_app
 from paths import Paths
 
 # Set up logging
+os.makedirs(Paths.logs_dir, exist_ok=True)
 logging.basicConfig(
     filename=Paths.logs_dir + "/logs.txt",
     filemode='a',
