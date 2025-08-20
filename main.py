a7c4f360bc37536a3e33bef0b730a6e6c676ebda
Robert Leist <robert.leist@dfki.de>
2025-08-20 07:41:17 +0200

    feat: added logging to a txt file

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import hydra
 from app import create_app
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    filename="./logs.txt",
+    filemode='a',
+    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = create_app()
 
