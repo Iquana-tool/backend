14998cc2ff709403d2a4faeff7ac97ff8e0875dd
Robert Leist <robert.leist@dfki.de>
2025-09-25 12:00:23 +0200

    added a session state to keep track of important parameters and to simplify access.

--- app/routes/image_annotation_session.py
+++ app/routes/image_annotation_session.py
@@ -1,17 +1,66 @@
 import json
+from typing import List
+
 from fastapi import APIRouter
 from fastapi.websockets import WebSocket
 from logging import getLogger
 
+from pydantic import BaseModel, Field, field_validator, model_validator
 from pydantic_core import ValidationError
 
+from app.database import get_context_session
+from app.database.images import Images
+from app.database.masks import Masks
+from app.routes.contours import add_contour
+from app.routes.masks import create_mask
 from app.services.ai_services import prompted_segmentation as prompted_service
-from uuid import UUID
 from app.schemas.annotation_session import ServerMessageType, ClientMessageType, ServerMessage, ClientMessage
+from app.schemas.contours import ContourModel
 
 
 router = APIRouter(prefix="/annotation_session", tags=["annotation_session"])
 logger = getLogger(__name__)
+
+
+class AnnotationSessionState(BaseModel):
+    """ A class to track the state of the annotation session. """
+    image_id: int = Field(..., title="Image ID", description="The id of the image whose annotations are being done.")
+    mask_id: int = Field(default=None,
+                         init=False,  # Isnt initialized in the init
+                         title="Mask ID",
+                         description="The id of the mask who annotations are being added to.")
+    user_id: int = Field(...,
+                         title="User ID",
+                         description="The id of the user who is annotating.")
+    annotation_completion_enabled: bool = Field(...,
+                                                title="Annotation completion",
+                                                description="Whether the annotation completion is enabled.")
+    awaiting_response: dict[str, ClientMessageType] = Field(default_factory=dict,
+                                                            title="Messages await responses",
+                                                            description="A dict mapping from message ids to the expected"
+                                                                        " client message. This is needed for prompting"
+                                                                        " user input and continuing the loop.")
+    conflicts: dict[int, List[int]] = Field(default_factory=dict,
+                                            title="Object conflicts",
+                                            description="A dict mapping from object ids to a list of "
+                                                    "object ids that are conflicting with it. This means"
+                                                    " that the object for example overlaps with another.")
+
+    @field_validator("image_id", mode="before")
+    def validate_image_id(cls, value, values):
+        with get_context_session() as session:
+            if not session.query(Images).filter_by(image_id=value).exists():
+                raise ValidationError(f"Image ID {value} does not exist.")
+        return value
+
+    @model_validator(mode="after")
+    def validate_model(self):
+        """ Validate the model and fill fields that were not initialized yet."""
+        with get_context_session() as session:
+            if not session.query(Masks).filter_by(image_id=self.image_id).exists():
+                create_mask(self.image_id)
+            mask_id = session.query(Masks.id).filter_by(image_id=self.image_id).first()
+            self.mask_id = mask_id
 
 
 async def receive_msg(websocket: WebSocket) -> ClientMessage:
@@ -43,9 +92,11 @@ async def send_msg(websocket: WebSocket, message: ServerMessage):
     await websocket.send_json(message.model_dump_json())
 
 
-async def on_startup(image_id, user_uid: str) -> ServerMessage:
+async def on_startup(state: AnnotationSessionState) -> ServerMessage:
     """Function to be called at the start of an annotation session. Any initialization code can be placed here.
     """
+    user_id = state.user_id
+    image_id = state.image_id
     # Check for running backends
     running = []
     if not prompted_service.check_backend():
@@ -56,8 +107,8 @@ async def on_startup(image_id, user_uid: str) -> ServerMessage:
 
     # Initialize backends by uploading the image etc.
     failed_initializations = []
-    if not (await prompted_service.upload_image(user_uid, image_id))["success"]:
-        logger.error(f"Failed to upload image {image_id} for user {user_uid} to prompted segmentation backend.")
+    if not (await prompted_service.upload_image(user_id, image_id))["success"]:
+        logger.error(f"Failed to upload image {image_id} for user {user_id} to prompted segmentation backend.")
         failed_initializations.append("prompted_segmentation")
 
     logger.info("Annotation session initialized.")
@@ -77,7 +128,7 @@ async def on_startup(image_id, user_uid: str) -> ServerMessage:
 
 
 @router.websocket("/ws/annotation_session/user={user_id}&image={image_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: str, image_id: str):
+async def websocket_endpoint(websocket: WebSocket, user_id: int, image_id: int):
     """WebSocket endpoint to handle real-time image annotation sessions. The image annotation session takes multiple
         messages from the user as input to start tasks in the background.
         Client sent messages should be structured as JSON and should look like this: \n
@@ -103,9 +154,14 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str, image_id: str):
         :raises WebsocketException: If the WebSocket connection fails.
     """
     await websocket.accept()
+    state = AnnotationSessionState(
+        image_id=image_id,
+        user_id=user_id,
+        annotation_completion_enabled=False,
+    )
     try:
         # Call some functions on startup
-        response = await on_startup(image_id, user_id)
+        response = await on_startup(state)
         # Send a message about startup
         await send_msg(websocket, response)
         while True:
@@ -113,33 +169,33 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str, image_id: str):
             # Here we handle different types of messages based on their "type" field
             match client_msg.type:
                 case ClientMessageType.FOCUS_IMAGE:
-                    handle_focus_image(client_msg)
+                    handle_focus_image(websocket, client_msg, state)
                 case ClientMessageType.UNFOCUS_IMAGE:
-                    handle_unfocus_image(client_msg)
+                    handle_unfocus_image(websocket, client_msg, state)
                 case ClientMessageType.OBJECT_ADD:
-                    handle_object_add(client_msg)
+                    handle_object_add(websocket, client_msg, state)
                 case ClientMessageType.OBJECT_DELETE:
-                    handle_object_delete(client_msg)
+                    handle_object_delete(websocket, client_msg, state)
                 case ClientMessageType.OBJECT_MODIFY:
-                    handle_object_modify(client_msg)
+                    handle_object_modify(websocket, client_msg, state)
                 case ClientMessageType.AUTOMATIC_SELECT_MODEL:
-                    handle_automatic_select_model(client_msg)
+                    handle_automatic_select_model(websocket, client_msg, state)
                 case ClientMessageType.AUTOMATIC_SEGMENTATION:
-                    handle_automatic_segmentation(client_msg)
+                    handle_automatic_segmentation(websocket, client_msg, state)
                 case ClientMessageType.PROMPTED_SELECT_MODEL:
-                    handle_prompted_select_model(client_msg)
+                    handle_prompted_select_model(websocket, client_msg, state)
                 case ClientMessageType.PROMPTED_SEGMENTATION:
-                    handle_prompted_segmentation(client_msg)
+                    handle_prompted_segmentation(websocket, client_msg, state)
                 case ClientMessageType.COMPLETION_SELECT_MODEL:
-                    handle_completion_select_model(client_msg)
+                    handle_completion_select_model(websocket, client_msg, state)
                 case ClientMessageType.COMPLETION_ENABLE:
-                    handle_completion_enable(client_msg)
+                    handle_completion_enable(websocket, client_msg, state)
                 case ClientMessageType.COMPLETION_DISABLE:
-                    handle_completion_disable(client_msg)
+                    handle_completion_disable(websocket, client_msg, state)
                 case ClientMessageType.FINISH_ANNOTATION:
-                    handle_finish_annotation(client_msg)
+                    handle_finish_annotation(websocket, client_msg, state)
                 case ClientMessageType.OBJECT_CONFLICT_RESOLUTION:
-                    handle_object_conflict_resolve(client_msg)
+                    handle_object_conflict_resolve(websocket, client_msg, state)
                 case _:
                     # Ignore erroneous messages from the client
                     pass
@@ -150,71 +206,72 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str, image_id: str):
 
 
 
-def handle_focus_image(websocket, data):
+def handle_focus_image(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle the client sending a focus image request"""
     pass
 
 
-def handle_unfocus_image(websocket, data):
+def handle_unfocus_image(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle the client unfocussing."""
     pass
 
 
-def handle_object_add(websocket, data):
+def handle_object_add(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle adding an object to the mask."""
-    pass
+    contour = ContourModel.model_validate_json(data)
+    await add_contour()
 
 
-def handle_object_delete(websocket, data):
+def handle_object_delete(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle removing an object from the mask. """
     pass
 
 
-def handle_object_modify(websocket, data):
+def handle_object_modify(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle Modifying an object. """
     pass
 
 
-def handle_automatic_select_model(websocket, data):
+def handle_automatic_select_model(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle the selection of an automatic model. """
     pass
 
 
-def handle_automatic_segmentation(websocket, data):
+def handle_automatic_segmentation(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle segmentation using an automatic model. """
     pass
 
 
-def handle_prompted_select_model(websocket, data):
+def handle_prompted_select_model(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle the selection of a prompted model. """
     pass
 
 
-def handle_prompted_segmentation(websocket, data):
+def handle_prompted_segmentation(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle segmentation using a prompted model. """
     pass
 
 
-def handle_completion_select_model(websocket, data):
+def handle_completion_select_model(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle the selection of a completion model. """
     pass
 
 
-def handle_completion_enable(websocket, data):
+def handle_completion_enable(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle enabling of completion model. Leads to a state change. """
     pass
 
 
-def handle_completion_disable(websocket, data):
+def handle_completion_disable(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle disabling of completion model. Leads to a state change. """
     pass
 
 
-def handle_finish_annotation(websocket, data):
+def handle_finish_annotation(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle marking a mask as finished. """
     pass
 
 
-def handle_object_conflict_resolve(websocket, data):
+def handle_object_conflict_resolve(websocket: WebSocket, client_msg: ClientMessage, state: AnnotationSessionState):
     """ Handle how an object conflict should be resolved. """
     pass
