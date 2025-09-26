f52b3690230ee9f90c7fd771cc429a4a8ce6bc17
Robert Leist <robert.leist@dfki.de>
2025-09-26 11:15:52 +0200

    Adding all fields to contours for better transfer

--- app/schemas/contours.py
+++ app/schemas/contours.py
@@ -12,6 +12,13 @@ class ContourModel(BaseModel):
     label: int | None = Field(default=None, description="ID of the label of the mask. None for unlabelled contour.")
     parent_contour_id: int | None = Field(default=None, description="ID of the parent contour. None if the contour has "
                                                                     "no parent")
+    added_by: str = Field(default_factory=str, description="ID of the user or model who added this contour.")
+    temporary: bool = Field(default=False, description="Whether or not this contour is temporarily added (eg. if a model added it).")
+    area: float | None = Field(default=None, description="Area of the contour.")
+    perimeter: float | None = Field(default=None, description="Perimeter of the contour.")
+    circularity: float | None = Field(default=None, description="Circularity of the contour.")
+    diameters: List[float] | None = Field(default=None, description="List of diameters of the contour measured from "
+                                                                    "different angles.")
 
     @field_validator('x', 'y')
     def validate_coordinates(cls, value):
