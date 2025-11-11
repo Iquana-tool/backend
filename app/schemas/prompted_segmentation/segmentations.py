7eaa61fab4237ee15da533aff332a3ae6d276995
Robert Leist <robert.leist@dfki.de>
2025-11-11 10:38:57 +0100

    allow user_id with str and int

--- app/schemas/prompted_segmentation/segmentations.py
+++ app/schemas/prompted_segmentation/segmentations.py
@@ -43,7 +43,7 @@ class PromptedSegmentationWebsocketRequest(BaseModel):
     """ Model for 2D segmentation form data. """
     model_identifier: str = Field(..., title="Model identifier", description="Model identifier string. "
                                                                              "Used to select the model.")
-    user_id: str = Field(..., title="User ID", description="Unique identifier for the user.")
+    user_id: str | int = Field(..., title="User ID", description="Unique identifier for the user.")
     prompts: Prompts = Field(..., title="Prompts", description="Prompts for segmentation")
     previous_mask: list[list[bool]] | None = Field(None, title="Previous Mask",
                                                   description="Optional previous mask to provide context.")
