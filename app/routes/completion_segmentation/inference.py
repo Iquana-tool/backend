e6d1334d57d7e2090e3c9338baf17655a5db1e54
Robert Leist <robert.leist@dfki.de>
2025-11-26 11:30:50 +0100

    simplfied

--- app/routes/completion_segmentation/inference.py
+++ app/routes/completion_segmentation/inference.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.database.contours import Contours
 from app.database.images import Images
-from app.schemas.completion_segmentation.inference import CompletionRequest, CompletionBackendRequest
+from app.schemas.completion_segmentation.inference import CompletionMainAPIRequest, CompletionServiceRequest
 from app.schemas.contours import Contour
 from app.schemas.user import User
 from app.services.auth import get_current_user
@@ -17,36 +17,12 @@ router = APIRouter(prefix="/completion_segmentation", tags=["Completion Segmenta
 
 @router.post("/infer/image_id={image_id}")
 async def infer_completion(
-        request: CompletionRequest,
+        request: CompletionMainAPIRequest,
         user: User = Depends(get_current_user),
         db: Session = Depends(get_session),
 ):
     # First upload the image
     await completion_service.upload_image(user.username, request.image_id)
-    height, width = db.query(Images.height, Images.width).filter_by(id=request.image_id).first()
-    seeds = []
-    label = None
-    for contour_id in request.seed_contours:
-        contour_db = db.query(Contours).filter_by(id=contour_id).first()
-        if contour_db is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Contour with id {contour_id} not found! "
-                                                                              f"Completion request failed!")
-        contour_model = Contour.from_db(contour_db)
-        if label is None:
-            label = contour_model.label_id
-        else:
-            if not label == contour_model.label_id:
-                raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                                    detail="You are trying to run completion on contours of different labels. This is "
-                                           "not allowed!")
-        seeds.append(np.argwhere(contour_model.to_binary_mask(height, width)).flatten())
-
-    backend_req = CompletionBackendRequest(
-        model_key=request.model_registry_key,
-        user_id=user.username,
-        seeds=seeds,
-    )
-
     # Finally add the result to the db
-    response = await completion_service.infer_instances(backend_req)
+    response = await completion_service.infer_instances(request.to_service_request(db))
     pass
--- app/schemas/completion_segmentation/inference.py
+++ app/schemas/completion_segmentation/inference.py
@@ -1,15 +1,47 @@
 from typing import List
 
+import numpy as np
+from fastapi import HTTPException, status
 from pydantic import BaseModel, Field
 
+from app.database.contours import Contours
+from app.database.images import Images
+from app.schemas.contours import Contour
 
-class CompletionRequest(BaseModel):
+
+class CompletionMainAPIRequest(BaseModel):
     image_id: int = Field(...)
     seed_contours: List[int] = Field(...)
     model_registry_key: str = Field(...)
 
+    def to_service_request(self, db):
+        height, width = db.query(Images.height, Images.width).filter_by(id=self.image_id).first()
+        seeds = []
+        label = None
+        for contour_id in self.seed_contours:
+            contour_db = db.query(Contours).filter_by(id=contour_id).first()
+            if contour_db is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                    detail=f"Contour with id {contour_id} not found! "
+                                           f"Completion request failed!")
+            contour_model = Contour.from_db(contour_db)
+            if label is None:
+                label = contour_model.label_id
+            else:
+                if not label == contour_model.label_id:
+                    raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                                        detail="You are trying to run completion on contours of different labels. This is "
+                                               "not allowed!")
+            seeds.append(np.argwhere(contour_model.to_binary_mask(height, width)).flatten())
 
-class CompletionBackendRequest(BaseModel):
+        return CompletionServiceRequest(
+            model_key=self.model_registry_key,
+            user_id=self.user_id,
+            seeds=seeds
+        )
+
+
+class CompletionServiceRequest(BaseModel):
     model_key: str = Field(..., description="The key of the model.")
     user_id: str = Field(..., description="The user id of the model.")
     seeds: List[List[int]] = Field(..., description="Seeds is a list of lists of indices that specify which pixels "
