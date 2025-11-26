04dbab7d2a3defa429f907bb080d36ae10316fe4
Robert Leist <robert.leist@dfki.de>
2025-11-26 11:51:53 +0100

    fixed another bug

--- app/routes/contours.py
+++ app/routes/contours.py
@@ -274,33 +274,25 @@ async def add_contours(mask_id: int,
     Returns:
         dict: A dictionary containing the success status, message, and lists of added and failed contour IDs.
     """
-    failed = []
-    added_ids = []
+    added = []
     for contour_to_add in contours_to_add:
-        logger.info(f"Added {len(added_ids)} / {len(contours_to_add)} contours. Failed {len(failed)}")
+        logger.info(f"Added {len(added)} / {len(contours_to_add)} contours. Failed {len(failed)}")
         result = await add_contour(mask_id, contour_to_add, user, db)
-        if not result["success"]:
-            failed.append({
-                "contour": contour_to_add,
-                "error": result["message"]
-            })
-        else:
-            added_ids.append(result["contour_id"])
-    if failed:
+        if result["success"]:
+            added.append(result["added_contour"])
+    if len(added) < len(contours_to_add):
         return {
             "success": False,
-            "message": f"Added {len(added_ids)} contours. Failed to add {len(failed)} contours.",
+            "message": f"Added {len(added)} contours. Failed to add all {len(contours_to_add)} contours.",
             "mask_id": mask_id,
-            "failed": failed,
-            "added_ids": added_ids
+            "added_contours": added,
         }
     else:
         return {
             "success": True,
             "message": "All contours added successfully.",
             "mask_id": mask_id,
-            "added_ids": added_ids,
-            "failed": []
+            "added_contours": added,
         }
 
 
