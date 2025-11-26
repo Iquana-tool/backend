a3e517496544fc22b7e00a710887d1029d831687
Robert Leist <robert.leist@dfki.de>
2025-11-26 11:36:14 +0100

    simplfied

--- app/routes/completion_segmentation/inference.py
+++ app/routes/completion_segmentation/inference.py
@@ -10,7 +10,7 @@ from app.schemas.user import User
 from app.services.auth import get_current_user
 from app.database import get_session
 from app.services.ai_services import completion_segmentation as completion_service
-
+from app.services.contours import contour_ids_to_indices
 
 router = APIRouter(prefix="/completion_segmentation", tags=["Completion Segmentation"])
 
@@ -23,6 +23,14 @@ async def infer_completion(
 ):
     # First upload the image
     await completion_service.upload_image(user.username, request.image_id)
+
+    seeds = contour_ids_to_indices(request.image_id, request.seed_contour_ids, db)
+
+    service_request = CompletionServiceRequest(
+        model_key=request.model_key,
+        user_id=user.username,
+        seeds=seeds,
+    )
     # Finally add the result to the db
-    response = await completion_service.infer_instances(request.to_service_request(db))
+    response = await completion_service.infer_instances(service_request)
     pass
--- app/schemas/completion_segmentation/inference.py
+++ app/schemas/completion_segmentation/inference.py
@@ -10,35 +10,9 @@ from app.schemas.contours import Contour
 
 
 class CompletionMainAPIRequest(BaseModel):
-    image_id: int = Field(...)
-    seed_contours: List[int] = Field(...)
-    model_registry_key: str = Field(...)
-
-    def to_service_request(self, db):
-        height, width = db.query(Images.height, Images.width).filter_by(id=self.image_id).first()
-        seeds = []
-        label = None
-        for contour_id in self.seed_contours:
-            contour_db = db.query(Contours).filter_by(id=contour_id).first()
-            if contour_db is None:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                    detail=f"Contour with id {contour_id} not found! "
-                                           f"Completion request failed!")
-            contour_model = Contour.from_db(contour_db)
-            if label is None:
-                label = contour_model.label_id
-            else:
-                if not label == contour_model.label_id:
-                    raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                                        detail="You are trying to run completion on contours of different labels. This is "
-                                               "not allowed!")
-            seeds.append(np.argwhere(contour_model.to_binary_mask(height, width)).flatten())
-
-        return CompletionServiceRequest(
-            model_key=self.model_registry_key,
-            user_id=self.user_id,
-            seeds=seeds
-        )
+    image_id: int | None = Field(...)
+    seed_contour_ids: List[int] = Field(...)
+    model_key: str = Field(...)
 
 
 class CompletionServiceRequest(BaseModel):
