ef0530ba17b44e5ed23741248af44f0774b0be5f
Robert Leist <robert.leist@dfki.de>
2026-01-30 16:08:59 +0100

    fix for fetching models

--- app/routes/services/completion_router.py
+++ app/routes/services/completion_router.py
@@ -14,12 +14,7 @@ router = APIRouter(prefix="/completion_segmentation", tags=["Completion Segmenta
 @router.get("/models")
 async def get_available_models(user: User = Depends(get_current_user)):
     """Retrieve the list of available prompted segmentation models from the backend."""
-    response = await completion_service.get_models()
-    return {
-        "success": True,
-        "message": "Retrieved available prompted segmentation models.",
-        "response": response
-    }
+    return await completion_service.get_models()
 
 
 @router.post("/run")
