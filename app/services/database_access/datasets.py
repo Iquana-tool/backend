b6cab98e6f5fd4fda643f0aaed10299cc7dac0b1
Robert Leist <robert.leist@dfki.de>
2026-02-27 10:21:00 +0100

    refactor: all datasets db methods moved to separate file; endpoints simplified massively

--- app/services/database_access/datasets.py
+++ app/services/database_access/datasets.py
@@ -1,23 +1,185 @@
+import os
+import shutil
+from collections import defaultdict
 from logging import getLogger
+from typing import Literal
 
 import numpy as np
 import pandas as pd
+from fastapi import Depends
 from iquana_toolbox.schemas.contours import Contour
+from iquana_toolbox.schemas.image import Image
 from iquana_toolbox.schemas.labels import LabelHierarchy
 from sqlalchemy.orm import Session
 
+from app.database import get_session
 from app.database.contours import Contours
+from app.database.datasets import Datasets
 from app.database.images import Images
 from app.database.labels import Labels
 from app.database.masks import Masks
+from app.database.users import Users
 from app.services.database_access.labels import get_hierarchical_label_name
+from config import DATASETS_DIR
 
 logger = getLogger(__name__)
+
+
+async def create_new_dataset(
+        name: str,
+        description: str,
+        owner_username: str,
+        db: Session = Depends(get_session)
+):
+    # Check if dataset with the same name already exists
+    existing_dataset = db.query(Datasets).filter_by(name=name.strip()).first()
+    if existing_dataset:
+        return {"success": False,
+                "message": f"Dataset with name '{name.strip()}' already exists.",
+                "error": "Duplicate dataset name"}
+
+    dataset_path = os.path.join(DATASETS_DIR, name.strip())
+    # Use exist_ok=True to avoid FileExistsError if directory already exists
+    os.makedirs(dataset_path, exist_ok=True)
+
+    new_dataset = Datasets(
+        name=name.strip(),
+        description=description.strip(),
+        folder_path=dataset_path,
+        dataset_type="image",
+        created_by=owner_username,
+    )
+    db.add(new_dataset)
+    db.commit()
+    db.refresh(new_dataset)
+    return new_dataset
+
+
+async def share_dataset(
+        dataset_id: int,
+        share_with_username: str,
+        sharing_username: str,
+        db: Session = Depends(get_session)
+):
+    dataset = db.query(Datasets).filter_by(id=dataset_id).first()
+    if dataset.created_by != sharing_username:
+        raise PermissionError("Only the owner can share a dataset")
+    user_to_share = db.query(Users).filter_by(username=share_with_username).first()
+    if not user_to_share:
+        raise ValueError("User to share with not found")
+    if not user_to_share in dataset.shared_with:
+        dataset.shared_with.append(user_to_share)
+        db.commit()
+
+
+async def user_has_sharing_permission_for_dataset(
+        dataset_id: int,
+        sharing_username: str,
+        db: Session = Depends(get_session)
+):
+    """ Check whether a user can share a dataset. """
+    dataset = db.query(Datasets).filter_by(id=dataset_id).first()
+    return sharing_username == dataset.created_by
+
+
+async def get_dataset(
+        dataset_id: int,
+        db: Session = Depends(get_session)
+):
+    return db.query(Datasets).filter_by(id=dataset_id).first()
+
+
+async def get_num_of_images_in_dataset(
+        dataset_id: int,
+        db: Session = Depends(get_session)
+):
+    return db.query(Images).filter_by(dataset_id=dataset_id).count()
+
+
+async def get_annotation_progress_of_dataset(
+        dataset_id: int,
+        db: Session = Depends(get_session)
+):
+    masks = (
+        db.query(Masks)
+        .join(Images, Masks.image_id == Images.id)
+        .filter(Images.dataset_id == dataset_id).all()
+    )
+    status_dict = defaultdict(lambda: 0)
+    for mask in masks:
+        status_dict[mask.status] += 1
+    return status_dict, len(masks)
+
+
+async def get_datasets_of_user(
+        user_name: str,
+        db: Session = Depends(get_session)
+):
+    return db.query(Users).filter_by(username=user_name).one().accessible_datasets
 
 
 async def get_label_hierarchy_of_dataset(dataset_id: int, db: Session) -> LabelHierarchy:
     labels = db.query(Labels).filter_by(dataset_id=dataset_id)
     return LabelHierarchy.from_query(labels)
+
+
+async def has_dataset_deletion_permission(
+        dataset_id: int,
+        username: str,
+        db: Session = Depends(get_session)
+):
+    raise NotImplementedError
+
+async def delete_dataset(
+        dataset_id: int,
+        db: Session = Depends(get_session)
+):
+    dataset = db.query(Datasets).filter_by(id=dataset_id).first()
+    if not dataset:
+        return {"success": False, "message": "Dataset not found."}
+    dataset_folder = str(dataset.folder_path)
+    # Delete the dataset
+    db.delete(dataset)
+    db.commit()
+    # Delete disk directory, removes all image files.
+    shutil.rmtree(dataset_folder, ignore_errors=True)
+
+
+async def get_image_and_mask_ids_of_dataset(
+        dataset_id: int,
+        filter_for_status: Literal["not_started", "in_progress", "reviewable", "finished"] | None = None,
+        db: Session = Depends(get_session),
+):
+    query = db.query(Images, Masks).join(Masks, Images.id == Masks.image_id).filter(Images.dataset_id == dataset_id)
+    if filter_for_status:
+        query = query.filter(Masks.status == filter_for_status)
+    result = query.all()
+    image_data = [
+        {
+            "image_id": img.id,
+            "mask_id": mask.id,
+            "status": mask.status
+        } for img, mask in result
+    ]
+    return image_data
+
+
+async def get_images_of_dataset(
+        dataset_id: int,
+        limit: int = None,
+        as_thumbnail: bool = False,
+        as_base64: bool = False,
+        db: Session = Depends(get_session)
+):
+    response = {}
+    images_query = db.query(Images).filter_by(dataset_id=dataset_id).limit(limit).all()
+    images = [Image.from_db(img) for img in images_query]
+    for img in images:
+        if as_thumbnail:
+            response[img.id] = img.load_thumbnail(as_base64=as_base64)
+        else:
+            response[img.id] = img.load_image(as_base64=as_base64)
+    return response
 
 
 async def get_dataset_as_df(
